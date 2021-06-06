c1930a2adc70f2eee8a969931d8f7e25bfe3f39a
dervis.kopuz <derviskopuz@gmail.com>
2021-06-06 23:57:03 +0300

    needs suspension

--- main.c
+++ main.c
@@ -18,10 +18,13 @@
 //  for handling signals
 #include <signal.h>
 
+#include <pthread.h>
+
 key_t READNEWS = 363;
 key_t PUBLISHED = 364;
 key_t KEYSHM = 365;
 key_t BOOLSHM = 366;
+key_t FETCHED = 367;
 
 //news source processes
 int m = 3;
@@ -34,10 +37,13 @@ char d;
 int messageMemory = sizeof(d);
 int arrayMemory = (sizeof(int)*n);
 
+int fetched[5];
+
 int shmid = 0;
 
 int publishSem = 0;
 int readNewsSem = 0;
+int fetchedSem = 0;
 
 //increase semaphore value
 void sem_signal(int semid, int val)
@@ -73,6 +79,8 @@ void publish(char d) {
     publishSem = semget(PUBLISHED, 1, 0);
     //get READNEWS semaphore
     readNewsSem = semget(READNEWS, 1, 0);
+    //get fetched semaphore
+    fetchedSem = semget(FETCHED, 1, 0);
     //try to decrease published semaphore by 1
     //if it is already 0, previous news wasnt fetched by all subscribers
     sem_wait(publishSem, 1);
@@ -84,14 +92,20 @@ void publish(char d) {
     //write message in memory
     *(messageMemoryPtr) = d;
     printf("published the message: %c \n", d);
+
+    sem_wait(fetchedSem,1);
     //set boolean array to all 0's
-    int boolShmid = shmget(BOOLSHM, arrayMemory, 0);
-    int* arrayMemoryPtr = (int*) shmat(boolShmid,0,0);
-    for (int i = 0; i<n; i++) {
-        *(arrayMemoryPtr + (sizeof(int) * i)) = 0;
+    for(int i = 0; i < n; i++) {
+        fetched[i] = 0;
     }
+    sem_signal(fetchedSem,1);
+    //int boolShmid = shmget(BOOLSHM, arrayMemory, 0);
+    //int* arrayMemoryPtr = (int*) shmat(boolShmid,0,0);
+    //for (int i = 0; i<n; i++) {
+      //  *(arrayMemoryPtr + (sizeof(int) * i)) = 0;
+    //}
     //detach the array memory segment
-    shmdt(arrayMemoryPtr);
+    //shmdt(arrayMemoryPtr);
     //set readnews sem to subscriber count
     sem_signal(readNewsSem, n);
 
@@ -103,6 +117,10 @@ void publish(char d) {
 void read_news() {
     //get READNEWS semaphore
     readNewsSem = semget(READNEWS, 1, 0);
+    //get PUBLISHED semaphore
+    publishSem = semget(PUBLISHED, 1, 0);
+    //get fetched semaphore
+    fetchedSem = semget(FETCHED, 1, 0);
 
     //get message shared memory
     shmid = shmget(KEYSHM, messageMemory, 0);
@@ -116,11 +134,46 @@ void read_news() {
     shmdt(messageMemoryPtr);
 
     //decrease readnews sem by 1
-    sem_wait(readNewsSem, 1);
-    printf("readnesem: %d \n", readNewsSem);
+    //sem_wait(readNewsSem, 1);
 
     //if readnews == 0
+    //increase publish sem when all subs read the news
+    //if fetched[] all 1's
+    sem_wait(fetchedSem,1);
+    int allRead = 1;
+    for( int i = 0; i<n; i++) {
+        if(fetched[i] == 0) {
+            //not every sub received the news
+            allRead = 0;
+        }
+    }
+    if(allRead == 1) {
+        //new news can be published
+        sem_signal(publishSem,1);
+    }
+    sem_signal(fetchedSem,1);
     
+}
+
+void* publisher(void* message) {
+    char mes = (char) message;
+    publish(mes);
+}
+
+void* reader(void* number) {
+    while (1) {
+        int num = (int) number;
+        //get fetched semaphore
+        fetchedSem = semget(FETCHED, 1, 0);
+        if(fetched[num] == 0) {
+            fetched[num] = 1;
+            read_news();
+            //set fetched[num] as read/1
+        } else {
+            //suspend thread
+            printf("No new message for subscriber %d \n", num);
+        }
+    }
 }
 
 
@@ -136,59 +189,67 @@ int main(int argc, char* argv[]) {
 
     int zero = 0;
     //create shared memory with subscriber numbers / for bool array (fetched)
-    int arrayShmid = shmget(BOOLSHM, arrayMemory, IPC_CREAT | 0700);
+    //int arrayShmid = shmget(BOOLSHM, arrayMemory, IPC_CREAT | 0700);
     //attach shared memory
-    memoryPtr = (int *) shmat(arrayShmid, 0, 0);
+    //memoryPtr = (int *) shmat(arrayShmid, 0, 0);
     //initialize bool array as 0
-    for (int i = 0; i<n; i++) {
-        *(memoryPtr + (sizeof(int) * i)) = zero;
+    for(int i = 0; i < n; i++) {
+        fetched[i] = zero;
     }
+    //for (int i = 0; i<n; i++) {
+      //  *(memoryPtr + (sizeof(int) * i)) = zero;
+    //}
     //detach the shared memory segment
-    shmdt(memoryPtr);
+    //shmdt(memoryPtr);
 
     //create PUBLISHED SEMAPHORE
     //initialized as 1 as there can only be 1 news 
     publishSem = semget(PUBLISHED, 1, 0700|IPC_CREAT);
     semctl(publishSem, 0, SETVAL, 1);
-    printf("pubbb %d", publishSem);
 
     //create READNEWS semaphore
     //initialized as 0. The number of subscribers
     readNewsSem = semget(READNEWS, 1, 0700|IPC_CREAT);
     semctl(readNewsSem, 0, SETVAL, 0);
-    printf("reaaaaaad %d \n", readNewsSem);
 
-    publish('x');
-    read_news();
-    read_news();
-    read_news();
-    read_news();
-    read_news();
-    semValue(readNewsSem);
-    semValue(readNewsSem);
-    semValue(readNewsSem);
+    //create fetched SEMAPHORE
+    //initialized as 1 as there can only be 1 news 
+    fetchedSem = semget(FETCHED, 1, 0700|IPC_CREAT);
+    semctl(fetchedSem, 0, SETVAL, 1);
+
+    //publish('x');
+    //read_news();
+    //read_news();
+    //read_news();
+    //read_news();
+    //read_news();
+    //semValue(readNewsSem);
+    //semValue(readNewsSem);
+    //semValue(readNewsSem);
     //need to create processes with fork in loop
 
-    int result;
-    int i;
-    //  create 2 child processes
-    for (i = 0; i < 2; i++)
-    {
-        result = fork();
-        if (result < 0)
-        {
-            printf("FORK error...\n");
-            exit(1);
-        }
-        if (result == 0) {
-            break;
-        } else {
-            printf("number:  %d \n", result);
-            //publish('y');
-            //read_news();
-        }
-    }
+    pthread_t pub1, pub2, pub3, read1,read2,read3,read4,read5;  //  create threads 
+    pthread_create(&pub1, NULL, publisher, (void*)'u');
+    pthread_create(&pub2, NULL, publisher, (void*)'o');
+    pthread_create(&pub3, NULL, publisher, (void*)'k');
+    //for (int k = 0; k<n; k++) {
+      //  pthread_create(&read1, NULL, reader, (void*) k);
+    //}
+    pthread_create(&read1, NULL, reader, (void*) 0);
+    pthread_create(&read2, NULL, reader, (void*) 1);
+    pthread_create(&read3, NULL, reader, (void*) 2);
+    pthread_create(&read4, NULL, reader, (void*) 3);
+    pthread_create(&read5, NULL, reader, (void*) 4);
 
+    //  wait for threads to finish
+    pthread_join(pub1, NULL);
+    pthread_join(pub2, NULL);
+    pthread_join(pub3, NULL);
+    pthread_join(read1, NULL);
+    pthread_join(read2, NULL);
+    pthread_join(read3, NULL);
+    pthread_join(read4, NULL);
+    pthread_join(read5, NULL);
 
     return 0;
 }
